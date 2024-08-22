bb0eb98d3e3c88c28182fed2a9936da8f1582b79
Patryk Małek <patryk.malek@konghq.com>
2024-08-22 17:43:13 +0200

    chore: add godocs (#13)
    * chore: add godocs
    
    * Bump .speakeasy/gen.yaml based on label
    
    * ci: go mod tidy
    
    * Update provider based on openapi.yaml changes

--- sdk.go
+++ sdk.go
@@ -261,9 +261,9 @@ func New(opts ...SDKOption) *SDK {
 		sdkConfiguration: sdkConfiguration{
 			Language:          "go",
 			OpenAPIDocVersion: "0.0.1",
-			SDKVersion:        "0.0.2",
-			GenVersion:        "2.404.1",
-			UserAgent:         "speakeasy-sdk/go 0.0.2 2.404.1 0.0.1 github.com/Kong/sdk-konnect-go",
+			SDKVersion:        "0.0.5",
+			GenVersion:        "2.404.2",
+			UserAgent:         "speakeasy-sdk/go 0.0.5 2.404.2 0.0.1 github.com/Kong/sdk-konnect-go",
 			Hooks:             hooks.New(),
 		},
 	}
