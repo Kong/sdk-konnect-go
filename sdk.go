e7f30199cc85849bd7a54e77f405d360d9b20120
Patryk Małek <patryk.malek@konghq.com>
2024-08-22 15:48:01 +0200

    ci: add basic CI

--- sdk.go
+++ sdk.go
@@ -261,9 +261,9 @@ func New(opts ...SDKOption) *SDK {
 		sdkConfiguration: sdkConfiguration{
 			Language:          "go",
 			OpenAPIDocVersion: "0.0.1",
-			SDKVersion:        "0.6.2",
+			SDKVersion:        "0.0.2",
 			GenVersion:        "2.404.1",
-			UserAgent:         "speakeasy-sdk/go 0.6.2 2.404.1 0.0.1 github.com/Kong/sdk-konnect-go",
+			UserAgent:         "speakeasy-sdk/go 0.0.2 2.404.1 0.0.1 github.com/Kong/sdk-konnect-go",
 			Hooks:             hooks.New(),
 		},
 	}
