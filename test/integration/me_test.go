200f75f6f05ce8e7d6d777f41999f596e60bfae3
Patryk Małek <patryk.malek@konghq.com>
2024-12-17 12:44:18 +0100

    tests: use KONG_CUSTOM_DOMAIN (#32)

--- test/integration/me_test.go
+++ test/integration/me_test.go
@@ -5,22 +5,15 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
-
-	sdkkonnectops "github.com/Kong/sdk-konnect-go/models/operations"
 )
 
 func TestMeOrganizations(t *testing.T) {
 	t.Parallel()
 
 	sdk := SDK(t)
-	url := KonnectURL(t)
-
 	ctx := context.Background()
-	respOrg, err := sdk.Me.GetOrganizationsMe(ctx,
-		// NOTE: This is needed because currently the SDK only lists the prod global API as supported:
-		// https://github.com/Kong/sdk-konnect-go/blob/999d9a987e1aa7d2e09ac11b1450f4563adf21ea/models/operations/getorganizationsme.go#L10-L12
-		sdkkonnectops.WithServerURL(url),
-	)
+	respOrg, err := sdk.Me.GetOrganizationsMe(ctx)
+
 	require.NoError(t, err)
 	require.NotNil(t, respOrg)
 	require.NotEmpty(t, respOrg.MeOrganization.ID)
@@ -33,15 +26,10 @@ func TestMeUsers(t *testing.T) {
 	t.Parallel()
 
 	sdk := SDK(t)
-	url := KonnectURL(t)
-
 	ctx := context.Background()
-	respOrg, err := sdk.Me.GetUsersMe(ctx,
-		// NOTE: This is needed because currently the SDK only lists the prod global API as supported:
-		// https://github.com/Kong/sdk-konnect-go/blob/999d9a987e1aa7d2e09ac11b1450f4563adf21ea/models/operations/getorganizationsme.go#L10-L12
-		sdkkonnectops.WithServerURL(url),
-	)
+	respOrg, err := sdk.Me.GetUsersMe(ctx)
 	require.NoError(t, err)
+
 	require.NotNil(t, respOrg)
 	require.NotEmpty(t, respOrg.User.ID)
 	require.NotNil(t, respOrg.User.FullName)
