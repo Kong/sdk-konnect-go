52708fcc61024d3035aec668df66348673a0b8d0
kong-apiops <122612077+kong-apiops@users.noreply.github.com>
2025-02-11 12:24:31 +0000

    feat(sdk): automated oas update (#73)
    * feat(sdk): automated oas update
    
    * Bump .speakeasy/gen.yaml based on label
    
    * Update provider based on openapi.yaml changes
    
    ---------
    
    Co-authored-by: pmalek <pmalek@users.noreply.github.com>
    Co-authored-by: team-k8s-bot <team-k8s-bot@users.noreply.github.com>

--- sdk.go
+++ sdk.go
@@ -269,9 +269,9 @@ func New(opts ...SDKOption) *SDK {
 		sdkConfiguration: sdkConfiguration{
 			Language:          "go",
 			OpenAPIDocVersion: "0.0.1",
-			SDKVersion:        "0.2.8",
+			SDKVersion:        "0.2.9",
 			GenVersion:        "2.506.0",
-			UserAgent:         "speakeasy-sdk/go 0.2.8 2.506.0 0.0.1 github.com/Kong/sdk-konnect-go",
+			UserAgent:         "speakeasy-sdk/go 0.2.9 2.506.0 0.0.1 github.com/Kong/sdk-konnect-go",
 			Hooks:             hooks.New(),
 		},
 	}
