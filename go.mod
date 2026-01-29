d8f0b3319b5ae054e79ce543ca788074b1234ce9
kong-apiops <122612077+kong-apiops@users.noreply.github.com>
2026-01-29 13:58:35 +0000

    feat(sdk): automated oas update (#257)
    * feat(sdk): automated oas update
    
    * Bump .speakeasy/gen.yaml based on label
    
    * Update provider based on openapi.yaml changes
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/Kong/sdk-konnect-go
 go 1.22
 
 require (
+	github.com/itchyny/gojq v0.12.17
 	github.com/spyzhov/ajson v0.8.0
 	github.com/stretchr/testify v1.11.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/itchyny/timefmt-go v0.1.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- sdk.go
+++ sdk.go
@@ -2,7 +2,7 @@
 
 package sdkkonnectgo
 
-// Generated from OpenAPI doc version 3.1.0 and generator version 2.801.2
+// Generated from OpenAPI doc version 3.1.0 and generator version 2.801.0
 
 import (
 	"context"
@@ -441,9 +441,9 @@ func WithTimeout(timeout time.Duration) SDKOption {
 // New creates a new instance of the SDK with the provided options
 func New(opts ...SDKOption) *SDK {
 	sdk := &SDK{
-		SDKVersion: "0.20.0",
+		SDKVersion: "0.20.1",
 		sdkConfiguration: config.SDKConfiguration{
-			UserAgent:  "speakeasy-sdk/go 0.20.0 2.801.2 3.1.0 github.com/Kong/sdk-konnect-go",
+			UserAgent:  "speakeasy-sdk/go 0.20.1 2.801.0 3.1.0 github.com/Kong/sdk-konnect-go",
 			ServerList: ServerList,
 		},
 		hooks: hooks.New(),
